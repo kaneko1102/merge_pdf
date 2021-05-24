c360e0bbb0f224f6968c7e14d3cf6d97948c613c
kaneko1102 <kaneko-takuya@ed.tmu.ac.jp>
2021-05-24 22:13:20 +0900

    first commit

---
+++ FilenameForm.java
@@ -0,0 +1,45 @@
+// https://www.javadrive.jp/tutorial/jtextfield/index9.html
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Container;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+class FilenameForm extends JFrame implements ActionListener{
+  JLabel label;
+  JTextField text;
+
+  public static void main(String args[]){
+    
+    FilenameForm frame = new FilenameForm("MyTitle");
+    frame.setVisible(true);
+  }
+
+  FilenameForm(String title){
+    setTitle(title);
+    setBounds(100, 100, 500, 100);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    text = new JTextField(20);
+    JButton button = new JButton("Get");
+    button.addActionListener(this);
+
+    JPanel p = new JPanel();
+    p.add(text);
+    p.add(button);
+
+    label = new JLabel();
+
+    Container contentPane = getContentPane();
+    contentPane.add(p, BorderLayout.CENTER);
+    contentPane.add(label, BorderLayout.SOUTH);
+  }
+
+  public void actionPerformed(ActionEvent e){
+    label.setText(text.getText());
+  }
+}
