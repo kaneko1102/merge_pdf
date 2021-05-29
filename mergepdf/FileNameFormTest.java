1b5f96ee605d6614e3490f6bb9f0b0d49e2bb6d3
kaneko1102 <kaneko-takuya@ed.tmu.ac.jp>
2021-05-29 18:24:26 +0900

    update

---
+++ mergepdf/FileNameFormTest.java
@@ -0,0 +1,8 @@
+package mergepdf;
+
+public class FileNameFormTest {
+    public static void main(String[] args){
+        MergePDFOutput frame = new MergePDFOutput("test");
+        frame.setVisible(true);
+    }
+}
